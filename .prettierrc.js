054a1a56a8b2fcdf63552a25bfac374aba8506ce
kn1ver <ser1632228@gmail.com>
2026-04-11 19:00:52 +0400

    architecture prepair

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  printWidth: 100,
+  tabWidth: 2,
+  plugins: [
+    'prettier-plugin-organize-imports',
+    'prettier-plugin-tailwindcss',
+  ],
+  tailwindConfig: './tailwind.config.js',
+};
